9ff8d4a49247d8bc3fe1e3c68a3d8dd3f8afb951
Geraldo Humberto <geraldohum@gmail.com>
2026-01-03 16:25:31 -0300

    inclusão de mais arquivos iniciais

---
+++ Curso_Javascript/projetos/1_number/scripts.js
@@ -0,0 +1,10 @@
+console.log(typeof 5);
+console.log(typeof 12.7);
+console.log(typeof -123);
+console.log(typeof 39500);
+console.log(typeof -123332);
+console.log(typeof 0.123);
+console.log(2 * 2);
+console.log(2 * 4 - 3);
+console.log(8 / 4);
+console.log(5 + (2 * 4));
